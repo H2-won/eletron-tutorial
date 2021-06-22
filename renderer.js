91931994948fab814295140dc4b01db05e7db09e
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-06-22 12:34:47 +0900

    기본 세팅 및 모달 윈도우, 파일 드래그 앤 드롭 테스트 [21.06.22]

--- renderer.js
+++ renderer.js
@@ -4,3 +4,17 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const holder = document.getElementById("holder");
+holder.ondragover = () => {
+  return false;
+};
+holder.ondragleave = holder.ondragend = () => {
+  return false;
+};
+holder.ondrop = (e) => {
+  e.preventDefault();
+  for (let f of e.dataTransfer.files) {
+    console.log("File(s) you dragged here: ", f.path);
+  }
+  return false;
+};
