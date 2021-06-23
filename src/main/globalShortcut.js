ed8c291b77ce3c441f19e5e2df3823674c2363bc
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-06-23 16:25:12 +0900

    globalShortcut test [21.06.23]

---
+++ src/main/globalShortcut.js
@@ -0,0 +1,16 @@
+const { app, globalShortcut, dialog } = require("electron");
+
+app.on("ready", () => {
+  globalShortcut.register("CommandOrControl+Alt+K", () => {
+    dialog.showMessageBox({
+      type: "info",
+      message: "Success!",
+      detail: "You pressed the registered global shortcut keybinding.",
+      buttons: ["OK"],
+    });
+  });
+});
+
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
+});
