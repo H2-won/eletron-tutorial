96908fe65ad058691a5019249a1d3d6d3adf3989
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-08-02 19:03:00 +0900

    app hide/show - 'close' event

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Tray, Menu } = require("electron");
 const path = require("path");
 const glob = require("glob");
 
+let tray = null;
+let mainWindow = null;
+
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1080,
     height: 720,
     webPreferences: {
@@ -16,17 +19,27 @@ function createWindow() {
     // 상단 메뉴바 숨기기, Alt 누르면 나타남
     autoHideMenuBar: true,
     // 작업표시줄에 아이콘 안뜨게
-    skipTaskbar: true,
+    skipTaskbar: false,
   });
-
   // and load the index.html of the app.
   mainWindow.loadFile("index.html");
-
   // 작업표시줄 아이콘에 작은 이미지 띄워줄때 (디스코드 빨간불 느낌인듯)
   mainWindow.setOverlayIcon("./huiIcon.png", "Description for overlay");
 
+  // close 버튼 누를 시, 종료하지말고 hide
+  mainWindow.on("close", function (e) {
+    e.preventDefault();
+    mainWindow.hide();
+  });
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+
+  // Create background window.
+  backgroundWindow = new BrowserWindow({
+    show: false,
+    webPreferences: { nodeIntegration: true },
+  });
+  backgroundWindow.loadFile("background.html");
 
   // Modal window 부모 윈도우 비활성화하면서 모달 띄움
   // let child = new BrowserWindow({
@@ -45,6 +58,7 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   loadMainProcessFiles();
+  createTrayIcon();
   createWindow();
 
   app.on("activate", function () {
@@ -53,6 +67,22 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
+
+// 트레이 아이콘
+function createTrayIcon() {
+  tray = new Tray("./huiIcon.png"); // 현재 애플리케이션 디렉터리를 기준으로 하려면 `__dirname + '/images/tray.png'` 형식으로 입력해야 합니다.
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" },
+  ]);
+  tray.setToolTip("이것은 나의 애플리케이션 입니다!");
+  tray.setContextMenu(contextMenu);
+  tray.on("double-click", () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+  });
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
